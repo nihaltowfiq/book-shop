1c2d7e461958d4159c3ed6848066f6dbd72123d3
nihaltowfiq <nihaltowfiq@gmail.com>
2021-07-13 01:21:58 +0600

    ready to start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { FC } from "react";
+import styled from "styled-components";
 
 const App: FC = () => {
-    return <div>Clean Up</div>;
+    return (
+        <Wrapper>
+            <h1>Clean Up</h1>
+        </Wrapper>
+    );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+    margin: auto;
+    text-align: center;
+`;
