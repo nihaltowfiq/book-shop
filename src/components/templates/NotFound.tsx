4ff21e8ab179d5d938a4ecd22abbf701a30ab85e
nihaltowfiq <nihaltowfiq@gmail.com>
2021-08-31 00:52:42 +0600

    not-found page updated

--- src/components/templates/NotFound.tsx
+++ src/components/templates/NotFound.tsx
@@ -1,11 +1,24 @@
+import { Image } from 'components/atoms';
 import { FC } from 'react';
+import styled from 'styled-components';
 import { MainLayout } from './Layouts';
 export const NotFound: FC = () => {
 	return (
 		<MainLayout>
-			<div>
-				<h3 className="text-center text-danger pt-5">sorry, page not found!</h3>
-			</div>
+			<Wrapper>
+				<Image src="/images/404.png" />
+				<h3 className="text-danger">sorry, page not found!</h3>
+			</Wrapper>
 		</MainLayout>
 	);
 };
+
+const Wrapper = styled.div`
+	text-align: center;
+	padding: 1rem;
+
+	img {
+		width: 400px;
+		height: 400px;
+	}
+`;
