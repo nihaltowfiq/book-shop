94bdb7fcce75bf9e5a230550d9f5d371af84d77c
nihaltowfiq <nihaltowfiq@gmail.com>
2021-09-11 23:57:28 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import { BookDetails, Home, Login, NotFound, Signup } from 'components/templates
 import { FC } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-const App: FC = () => {
-	return (
-		<Switch>
-			<Redirect from="/home" to="/" />
-			<Route exact path="/" component={Home} />
-			<Route path="/book/:bookID" component={BookDetails} />
-			<Route path="/login" component={Login} />
-			<Route path="/signup" component={Signup} />
-			<Route path="*" component={NotFound} />
-		</Switch>
-	);
-};
+const App: FC = () => (
+	<Switch>
+		<Redirect from="/home" to="/" />
+		<Route exact path="/" component={Home} />
+		<Route path="/book/:bookID" component={BookDetails} />
+		<Route path="/login" component={Login} />
+		<Route path="/signup" component={Signup} />
+		<Route path="*" component={NotFound} />
+	</Switch>
+);
 
 export default App;
